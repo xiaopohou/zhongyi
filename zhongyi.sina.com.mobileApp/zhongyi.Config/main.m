315753f70438d94c9d31fc64276b048956ac205d
xiaopohou <393331943@qq.com>
2016-06-28 17:10:20 +0800

    提交

---
+++ zhongyi.sina.com.mobileApp/zhongyi.Config/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  zhongyi.sina.com.mobileApp
+//
+//  Created by Monkey on 16/6/28.
+//  Copyright © 2016年 zhongyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ zhongyi.sina.com.mobileApp/zhongyi.Controller/Main/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zhongyi.sina.com.mobileApp
+//
+//  Created by Monkey on 16/6/28.
+//  Copyright © 2016年 zhongyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
